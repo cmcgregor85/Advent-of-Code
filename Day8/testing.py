0be268b52802f582ece1282e99633411ecaf92c1
Collin McGregor <cmcgregor@amyris.com>
2022-12-13 14:37:34 -0800

    Fixed bug - position was not being reset

--- Day8/testing.py
+++ Day8/testing.py
@@ -4,15 +4,17 @@ storedValue = 0
 value = 0
 position = 0
 numTrees = 0
-visible = bool('TRUE')
 visibleTrees = set()
+row = 1
+storedTree = ""
 while storedPosition<len(input):
-    storedValue = input[storedPosition]
+    storedValue = int(input[storedPosition])
     while position<=storedPosition:
         if position == storedPosition:
             numTrees+=1
-            visibleTrees.add(storedPosition)
-        value=input[position]
+            storedTree = str(row)+'-'+str(storedPosition)
+            visibleTrees.add(storedTree)
+        value=int(input[position])
         if value>=storedValue:
             break
         position+=1
@@ -21,13 +23,15 @@ while storedPosition<len(input):
     while position<len(input):
         if position == (len(input)-1):
             numTrees+=1
-            visibleTrees.add(storedPosition)
-        value=input[position]
+            storedTree = str(row)+'-'+str(storedPosition)
+            visibleTrees.add(storedTree)            
+        value=int(input[position])
         if value>=storedValue:
             break
         position+=1
 
     storedPosition+=1
+    position = 0
 
 print(visibleTrees)
 
