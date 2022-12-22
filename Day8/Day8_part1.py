78c5cf2026f93a2ef81a109d1e42b6c7874d12a0
Collin McGregor <cmcgregor@amyris.com>
2022-12-21 16:29:27 -0800

    Fixed file names

--- Day8/Day8_part1.py
+++ Day8/Day8_part1.py
@@ -1,38 +1,79 @@
-path = "C:/Users/cmcgregor/source/repos/AdventOfCode/Advent-of-Code/Day8/Day8_Input1.txt"
-trees = {}
-counter = 0
-xsize = 0
-ysize = 0
-leftVisibleTrees = []
-rightVisibleTrees = []
-topVisibleTrees = []
-bottomVisibleTrees = []
-#make a dictionary of all the trees and their heights?
-#with open(path) as inputFile:
-#    input = inputFile.read().splitlines()
-#    for line in input:
-#        xsize = len(line)
-#        for tree in line:
-#            trees[counter] = tree
-#            counter+=1
-#ysize = int(counter/xsize)
-#for tree in trees:
-#    if tree<xsize:
-#        topVisibleTrees.append(tree)
-#print(topVisibleTrees)
-
-treeCounter = 0
-currentTreeHeight = 0
-rowCounter = 0
+import numpy as np
+path = "C:/Users/cmcgregor/source/repos/AdventOfCode/Advent-of-Code/Day8/Day8_Input.txt"
+input = []
+storedPosition = 0
+storedValue = 0
+value = 0
+position = 0
+numTrees = 0
+visibleTrees = set()
+row = 1
+storedTree = ""
 with open(path) as inputFile:
-    input = inputFile.read().splitlines()
-    for row in input:
-        for tree in row:
-            currentTreePos = treeCounter
-            currentTreeHeight = row[currentTreePos]
-            for tree in row[:currentTreePos]:
-                if currentTreePos == treeCounter:
-                    break
-                treeCounter+=1
-                if currentTreeHeight>row[tree]:
-                    print('...waht')
+    input = np.genfromtxt(inputFile,dtype = str,delimiter=1)
+for i in input:
+    while storedPosition<len(i):
+        storedValue = int(i[storedPosition])
+        while position<=storedPosition:
+            if position == storedPosition:
+                storedTree = str(row)+'-'+str(storedPosition+1)
+                visibleTrees.add(storedTree)
+            value=int(i[position])
+            if value>=storedValue:
+                break
+            position+=1
+        position = storedPosition
+        
+        while position<len(i):
+            if position == (len(i)-1):
+                storedTree = str(row)+'-'+str(storedPosition+1)
+                visibleTrees.add(storedTree)  
+                break          
+            value=int(i[position+1])
+            if value>=storedValue:
+                break
+            position+=1
+
+        storedPosition+=1
+        position = 0
+    storedPosition = 0
+    position = 0
+    row+=1
+
+input = np.swapaxes(input,0,1)
+row=1
+for i in input:
+    while storedPosition<len(i):
+        storedValue = int(i[storedPosition])
+        while position<=storedPosition:
+            if position == storedPosition:
+                storedTree = str(storedPosition+1)+'-'+str(row)
+                visibleTrees.add(storedTree)
+            value=int(i[position])
+            if value>=storedValue:
+                break
+            position+=1
+        position = storedPosition
+        
+        while position<len(i):
+            if position == (len(i)-1):
+                storedTree = str(storedPosition+1)+'-'+str(row)
+                visibleTrees.add(storedTree)  
+                break          
+            value=int(i[position+1])
+            if value>=storedValue:
+                break
+            position+=1
+
+        storedPosition+=1
+        position = 0
+    storedPosition = 0
+    position = 0
+    row+=1
+
+for x in visibleTrees:
+    numTrees+=1
+print(numTrees)
+
+
+
